c9a5bf0f1d639d5387f113fbd1e116b046ecbdec
Mark Diehr <mdiehr@gmail.com>
2017-07-09 19:25:56 -0400

    Fix lots of extra spaces

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ const grammar = tracery.createGrammar(the_pros);
 
 grammar.addModifiers(tracery.baseEngModifiers); 
 
-for (let i = 0; i < 5; ++i) {
-    console.log(grammar.flatten('#origin#'));
+for (let i = 0; i < 10; ++i) {
+    console.log("\n * " + grammar.flatten('#origin#'));
 }
