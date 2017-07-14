1236652777983d47b3fef28d0b3da9f9f8a17cce
Mark Diehr <mdiehr@gmail.com>
2017-07-14 10:45:17 -0400

    Add file logging, check for tweets that are too long

--- bot.js
+++ bot.js
@@ -2,11 +2,30 @@
 const Twit = require('twit');
 const tracery = require(`tracery-grammar`);
 const the_pros = require(`./src/grammar.json`);
+const fs = require(`fs`);
+const logFile = `./debug.log`;
+const TWEET_LENGTH = 140;
 
 // Create tweet from grammar
 const grammar = tracery.createGrammar(the_pros);
 grammar.addModifiers(tracery.baseEngModifiers); 
-const tweet = grammar.flatten("#origin#");
+const ORIGIN = `#origin#`;
+const generate = () => grammar.flatten(ORIGIN);
+
+let fileLogger = (msg) => {
+    console.log(`${msg}`);
+    fs.appendFile(logFile, `${msg}\n`, (err) => {
+        if (err) throw err;
+    });
+};
+
+let tweet = generate();
+while (tweet.length > TWEET_LENGTH) {
+    fileLogger(`ERROR: TWEET LENGTH ${tweet.length} GREATER THAN MAX ${TWEET_LENGTH}:\n${tweet}`);
+    tweet = generate()
+}
+
+fileLogger(`${tweet}`);
 
 // Create twitter client
 var T = new Twit(
@@ -16,8 +35,6 @@ var T = new Twit(
     , access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET
     }
 );
-
-console.log(`Tweeting tweet:\n${tweet}`);
 
 T.post('statuses/update', { status: tweet }, function(err, data, response) {
     if (err) {
