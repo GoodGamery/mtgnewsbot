0b87e4514e83fc7b79069c91ce52eeb7985dc772
Justin Woolley <justinwoolley@gmail.com>
2017-07-15 05:51:59 -0700

    fully promisified example of image download,upload, and post

--- scratch/promise-upload-image-test.js
+++ scratch/promise-upload-image-test.js
@@ -8,9 +8,8 @@ function lazyGuid() {
 }
 
 const cardFinderUrl = 'https://forums.goodgamery.com/includes/mtg/mtg_helper_cardfinder_v3.php?img=true&find=';
-const cardname = 'Kookus';
-const imgurl = cardFinderUrl + cardname;
-const outputfile = cardname.replace(/\s/g, '-').toLowerCase() + '-' + lazyGuid() + '.jpg';
+const cardname = 'Three Visits';
+const outputfile = cardname.replace(/\s+/g, '-').toLowerCase() + '-' + lazyGuid() + '.jpg';
 const outputDir = 'logs';
 const outputPath = outputDir + '/' + outputfile;
     
@@ -22,10 +21,43 @@ var T = new Twit(
     }
 );
 
+function downloadMtgCardFinderImage(cardname, outputPath) {
+  return new Promise((resolve, reject) => {  
+    const imgurl = cardFinderUrl + cardname;    
+    try {
+      console.log('Downloading image data from ' + imgurl);
+      https.request(imgurl, function(response) {                                        
+        var data = new Stream();
+
+        response.on('data', function(chunk) {                                       
+          data.push(chunk);                                                         
+        });                                                                         
+
+        response.on('end', function() {
+          console.log('Writing image data file...');
+
+          fs.writeFileSync(outputPath, data.read());
+          console.log('Wrote image date to ' + outputPath);
+
+          resolve(outputPath);        
+        });
+
+        response.on('error', function(e) {                                             
+          reject(e);                         
+        });
+      }).end();
+    } catch(e) {
+      reject(e);
+    }
+  });
+}
+
 function uploadTwitterImage(filepath) {
    return new Promise((resolve, reject) => {
       try {
         var b64content = fs.readFileSync(filepath, { encoding: 'base64' });
+
+        console.log('Uploading image file to twitter...');
 
         T.post('media/upload', { media_data: b64content }, function (err, data, response) {
           if (!err) { 
@@ -40,55 +72,47 @@ function uploadTwitterImage(filepath) {
    });
 }
 
-function postImageTweet(filepath, altText, message) {
+function postImageTweet(data, altText, message) {
   return new Promise((resolve, reject) => {
-    // first we must post the media to Twitter
-    uploadTwitterImage(filepath)
-      .then(data =>{ 
-        // now we can assign alt text to the media, for use by screen readers and
-        // other text-based presentations and interpreters
-        var mediaIdStr = data.media_id_string;
-        var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+    // now we can assign alt text to the media, for use by screen readers and
+    // other text-based presentations and interpreters
+    var mediaIdStr = data.media_id_string;
+    var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
 
-        T.post('media/metadata/create', meta_params, function (err, data, response) {
-          if (!err) {
-            // now we can reference the media and post a tweet (media will attach to the tweet)
-            var params = { status: message, media_ids: [mediaIdStr] }
+    console.log('Setting media metadata...');
 
-            T.post('statuses/update', params, function (err, data, response) {
-              console.log(data);
-              resolve();
-            });
-          } else {
-            reject(err);
-          }
+    T.post('media/metadata/create', meta_params, function (err, data, response) {
+      if (!err) {
+        // now we can reference the media and post a tweet (media will attach to the tweet)
+        var params = { status: message, media_ids: [mediaIdStr] }
+
+        console.log('Posting tweet...');
+
+        T.post('statuses/update', params, function (err, data, response) {
+          console.log(data);
+          resolve();
         });
-      });
+      } else {
+        reject(err);
+      }
+    });
   });  
 }
 
-https.request(imgurl, function(response) {                                        
-  var data = new Stream();                                                    
-
-  response.on('data', function(chunk) {                                       
-    data.push(chunk);                                                         
-  });                                                                         
-
-  response.on('end', function() {                                             
-    fs.writeFileSync(outputPath, data.read());
-    postImageTweet(outputPath, cardname, 'The card you\'re thinking of is ' +  cardname)
-      .then(() => { console.log('Posted to twitter.') }, e => { console.err('Failed to post to twitter: ' + e); })
+downloadMtgCardFinderImage(cardname, outputPath)
+  .then(localFilePath => uploadTwitterImage(localFilePath), e => console.error('Failed to upload image: ' + e))
+  .then(twitterImage => {
+    return postImageTweet(twitterImage, cardname, 'The card you\'re thinking of is ' +  cardname)
+      .then(() => { 
+        console.log('Posted tweet.'); 
+      }, e => console.error('Failed to post tweet: ' + e))
       .then(() => {
-        fs.unlink(outputPath, (err) => {
-        if (err) throw err;
-          console.log('Deleted ' + outputPath);
+        return fs.unlink(outputPath, (err) => {
+          if (!err) { 
+            console.log('Deleted ' + outputPath);           
+          } else {
+            console.error('Unable to delete local image file: ' + err);            
+          }
         });   
-      });
-  });
-
-  response.on('error', function(e) {                                             
-    console.log('Failed to download image: ' + e);                         
-  });       
-
-}).end();
-
+      });    
+  }, e => console.error('Failed to download image: ' + e));
