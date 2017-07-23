cb4d629be7abfbf5e6c954283cc448b223ceb35f
Justin Woolley <justinwoolley@gmail.com>
2017-07-22 23:22:25 -0700

    working image cropping

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const config = require('../config');
 const fs =  require(`fs`);
 const uuid = require(`uuid`);
 const svg2png = require('svg2png');
+const html2png = require('html2png');
+const gm = require('gm');
 
 const numExamples = process.argv[2] || 30;
 const headlines = NewsEngine.generateHeadlines(numExamples);
@@ -23,5 +25,23 @@ headlines.forEach(headline => {
 				console.log('\n *** Image saved to ' + outputPath);
 			})
 			.catch(e => console.log('\n *** Failed to save image: ' + e));
+	}
+
+	if(headline.tags && headline.tags.htmlImg && headline.tags.htmlImg.htmlImgString) {
+		const outputfile = uuid() + '.png';
+		const outputPath = config.paths.tempDirectory + '/' + outputfile;
+		const screenshot = html2png({ width: 1024, height: 768, browser: 'phantomjs'});
+		const html = headline.tags.htmlImg.htmlImgString.replace(/`/g, '"');
+		console.log('HTML:'); console.log(html);
+
+		screenshot.render(html, function (err, data) { 
+			if (err) { console.log('\n *** Failed to create png: ' + err); return; }
+			gm(data).trim()
+			.write(outputPath,  err => {	
+				if (err) {  console.error('ERROR: ' + err); return err; }		
+				console.log('\n *** Image saved to ' + outputPath);
+				screenshot.close();
+			});
+		});	  
 	}
 });
