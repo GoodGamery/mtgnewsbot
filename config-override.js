fa7ecc5a85f02ad130a93d0d4958a5ac18dee18e
Justin Woolley <justinwoolley@gmail.com>
2017-07-26 00:39:22 -0700

    dial back logo frequency slightly

--- config-override.js
+++ config-override.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const config = {
-  origin: ["#headline#", "#headline#", "#headline#", "#headline#", "#headline#", "#headline#", "#headline#", "#graphicAnnouncement#"]
+  origin: ["#headline#", "#headline#", "#headline#", "#headline#", "#headline#", 
+  					"#headline#", "#headline#", "#headline#", "#headline#", "#graphicAnnouncement#"]
 };
 
 module.exports = config;
