7be988c463a837f195c142ea6d56eca005159919
Mark Diehr <mdiehr@gmail.com>
2017-08-01 20:45:30 -0400

    Separate the git pull from the tweeting of the bot

---
+++ tweet.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Export env vars for twitter tokens
+set -a
+. ./.env
+set +a
+
+#noisy
+set -x
+
+#log the path
+echo $PATH
+
+#log node version
+node -v
+
+# install fonts
+cp ./src/data/fonts/*.ttf /usr/local/share/fonts
+
+# install dependencies
+npm install --production
+
+# tweet
+node bot.js
+
+#silent
+set +x
