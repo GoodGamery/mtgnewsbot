7a01a53e81e80cfdcec8ec8f05fec2f540266f64
Mark Diehr <mdiehr@gmail.com>
2017-08-05 14:13:34 -0400

    Support flat text files

--- src/build-grammar.js
+++ src/build-grammar.js
@@ -12,14 +12,28 @@ function buildGrammar(directory) {
   const files = fs.readdirSync(directory);
   console.info(`Loading ${files.length} files:\n${files.join(', ')}`);
 
-  const rightFileType = s => s.endsWith(`.json`) || s.endsWith(`.yaml`);
+  const flatType = s => s.endsWith(`.txt`);
+  const yamlType = s => s.endsWith(`.json`) || s.endsWith(`.yaml`);
 
   let destObj = {};
 
   files
-    .filter(rightFileType)
-    .map(fileName => `${directory}/${fileName}`)
-    .map(filePath => fs.readFileSync(filePath))
+    .filter(flatType)
+    .map(fileName => {
+      const fileData = fs.readFileSync(`${directory}/${fileName}`);
+      const dataArray = fileData.toString()
+        .split(/\r\n/)
+        .filter(s => s !== ``);
+      let doc = {};
+      const key = fileName.slice(0, -4);
+      doc[key] = dataArray;
+      return doc;
+    })
+    .forEach(doc => destObj = deepMerge(destObj, doc));
+
+  files
+    .filter(yamlType)
+    .map(fileName => fs.readFileSync(`${directory}/${fileName}`))
     .map(fileData => yaml.safeLoad(fileData))
     .forEach(doc => destObj = deepMerge(destObj, doc));
 
