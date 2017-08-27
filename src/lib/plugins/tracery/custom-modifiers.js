7984329d2852efb2ab05cf407c60d062145bce0d
Justin Woolley <justinwoolley@gmail.com>
2017-08-27 02:41:10 -0700

    more cotd variations

--- src/lib/plugins/tracery/custom-modifiers.js
+++ src/lib/plugins/tracery/custom-modifiers.js
@@ -17,18 +17,58 @@ function hyphenate(s) {
   return s.replace(/\s+/g, '-');
 }
 
-async function cardSearchByType(s) {
-  
-  const SEARCH_API_JSON_URL = 'https://goodgamery.com/api/mtg/card/json';
-  
+async function cardSearchBySet(s) {
+  const query = { 
+    q: 'set:' + s,
+    limit: 1,
+    sort: 'random'
+  };
+  return cardFinderSearch(query);    
+}
+
+async function cardSearchByText(s) {
+  const query = { 
+    q: 'text:' + s,
+    limit: 1,
+    sort: 'random'
+  };
+  return cardFinderSearch(query);  
+}
+
+async function cardSearchByType(s) {  
   const query = { 
     q: 't:' + s,
     limit: 1,
     sort: 'random'
   };
+  return cardFinderSearch(query);
+}
 
-  console.log('[cardSearchByType] searching for card with type ' + s);
+function getColorDescription(colorIdentity) {
+  const colorNames = {
+    W: 'white',
+    U: 'blue',
+    B: 'black',
+    R: 'red',
+    G: 'green',
+    WU: 'UW',
+    WR: 'RW',
+    WG: 'GW',
+    BR: 'RB',
+    UBG: 'BUG',
+    URG: 'RUG'
+  };
 
+  if (!colorIdentity) {
+    return 'colorless';
+  }
+
+  const colorDescription = colorIdentity.join('');
+  return colorNames[colorDescription] || colorDescription;
+}
+
+async function cardFinderSearch(query) {
+  const SEARCH_API_JSON_URL = 'https://goodgamery.com/api/mtg/card/json';
   return new Promise(resolve => {
     request.get({ url: SEARCH_API_JSON_URL, qs: query }, (err, data, body) => {
       if (err) {
@@ -37,21 +77,23 @@ async function cardSearchByType(s) {
       }
 
       try {
-        // console.log('[cardSearchByType] response:\n' + JSON.stringify(data));
-        // console.log('[cardSearchByType] body:\n' + JSON.stringify(body));
+        console.log('[cardFinderSearch] reqdata:\n' + JSON.stringify(data));
+        // console.log('[cardFinderSearch] result:\n' + JSON.stringify(JSON.parse(body)[0], null, '\t'));
         const result = JSON.parse(body);
         const name = result[0].name;
         const imgUrl = result[0].imageUrl.replace(/:/g,'#colon#');
+        const color = getColorDescription(result[0].colorIdentity);
 
         console.log('name: ' + name);
         console.log('imageUrl: ' + imgUrl);
-        resolve(`[_cardName1:${name}][_cardImgUrl1:${imgUrl}]`);
+        console.log('color: ' + color);
+        resolve(`[_cardName1:${name}][_cardImgUrl1:${imgUrl}][_cardColor1:${color}]`);
       } catch (e) {
         console.warn('Error parsing card data: ' + e);
         resolve('NO RESULTS');
       }
     });
-  });
+  });  
 }
 
 module.exports = { 
@@ -59,5 +101,7 @@ module.exports = {
   hyphenate: hyphenate,
   noPunctuation: noPunctuation,
   noSpaces: noSpaces,
-  cardSearchByType: cardSearchByType
+  cardSearchBySet: cardSearchBySet,  
+  cardSearchByText: cardSearchByText,
+  cardSearchByType: cardSearchByType  
 };
