6f9ec3a68c309654a23f7034e119e0a9a35ba1b8
Justin Woolley <justinwoolley@gmail.com>
2017-08-29 00:18:36 -0700

    multicard search

--- src/lib/plugins/tracery/modifiers/cardfinder-modifiers.js
+++ src/lib/plugins/tracery/modifiers/cardfinder-modifiers.js
@@ -69,48 +69,46 @@ function getSomeCardTypeOrSubtype(types, subtypes) {
   return randomElement(typesAndSubtypes);
 }
 
-async function randomCard() {
+async function randomCards(limit) {
   const query = { 
     q: 'cmc >= 0',
-    limit: 1,
+    limit: limit || 1,
     sort: 'random'
   };
   return searchCardFinder(query);
 }
 
-function randomStaticCard() {
+function randomStaticCard(limit) {
   const cardData = readJsonFile(staticCardDataFile);
-  return randomElement(cardData);
+  const cards = [];
+  for (let i = 0; i < limit || 1; i++) {
+    cards.push(randomElement(cardData));
+  }
+  return cards;
 }
 
-async function cardSearchBySet(s) {
+async function cardSearchBySet(s, params) {
   const query = { 
-    q: 'set:' + s,
-    limit: 1,
-    sort: 'random'
+    q: 'set:' + s
   };
-  return cardFinderSearch(query);    
+  return cardFinderSearch(query, params);    
 }
 
-async function cardSearchByText(s) {
+async function cardSearchByText(s, params) {
   const query = { 
-    q: 'text:' + s,
-    limit: 1,
-    sort: 'random'
+    q: 'text:' + s
   };
-  return cardFinderSearch(query);  
+  return cardFinderSearch(query, params);  
 }
 
-async function cardSearchByType(s) {  
+async function cardSearchByType(s, params) {
   const query = { 
-    q: 't:' + s,
-    limit: 1,
-    sort: 'random'
+    q: 't:' + s
   };
-  return cardFinderSearch(query);
+  return cardFinderSearch(query, params);
 }
 
-async function searchCardFinder(query) {
+async function searchCardFinder(query, params) {
   const SEARCH_API_JSON_URL = 'https://goodgamery.com/api/mtg/card/json';
 
   return new Promise((resolve, reject) => {
@@ -128,18 +126,38 @@ function traceryEscape(string) {
   return string.replace(/:/g,'#colon#').replace(/,/g,"#comma#");
 }
 
-async function cardFinderSearch(query) {
+async function cardFinderSearch(query, params) {
   let resultData;
   let result;
+  let queryLimit = 1;
+
+  if (params.length > 0) {
+    if (parseInt(params[0]) > 0) {
+      queryLimit = parseInt(params[0]);
+    } else {
+      logger.warn(`Invalid query limit specified for query ${JSON.stringify(query)}: '${params[0]}' is not a valid positive integer.`)
+    }
+  }
+
+  query.limit = queryLimit;
+  query.sort = 'random';
+
+  logger.log(`Searching for ${queryLimit} cards.`);
   try {
-    try {
-     resultData = await searchCardFinder(query);
+    try {      
+      resultData = await searchCardFinder(query);
       try {
         result = JSON.parse(resultData);     
-        if (result.length === 0) {
-          console.warn('No results returned from card search. Fetching random card.');
-          resultData = await randomCard();
-          result = JSON.parse(resultData);
+        logger.log('Retrieved ' + result.length + ' cards.');
+
+        if (result.length < queryLimit) {          
+          logger.warn(`Zero or insufficient results returned from card search: expected ${queryLimit} but received ${result.length}.`);
+          logger.warn('Fetching random cards.');
+
+          resultData = await randomCards(queryLimit - result.length);
+          logger.log('Retrieved ' + JSON.parse(resultData).length + ' addtional cards.');
+
+          result = result.concat(JSON.parse(resultData));
         }
       } catch (e) {
         throw new Error('Error parsing card data: ' + e);    
@@ -149,51 +167,59 @@ async function cardFinderSearch(query) {
     }      
   } catch (e) {
     logger.warn(e);    
-    result = [randomStaticCard()];
+    result = [randomStaticCards(queryLimit)];
   }
-    
+   
+  logger.log('Result: ' + JSON.stringify(result));
+  logger.log('Total cards: ' + result.length);
 
-  try {
-    const card = result[0];
+  try {    
+    let finalResult = '';
+    for (let i = 1; i <= queryLimit; i++) {
+      const card = result[i - 1];
 
-    const name = traceryEscape(card.name);
-    const set = traceryEscape(card.set);
-    const rarity = traceryEscape(card.rarity);      
-    const type = randomElement(card.types);
-    const subtype = randomElement(card.subtypes);
-    const fullType = card.types.join(' ');
-    const fullSubtype = card.subtypes && card.types.join(' ');    
-    const someTypeOrSubtype = getSomeCardTypeOrSubtype(card.types, card.subtypes);
-    const imgUrl = traceryEscape(card.imageUrl);
-    const color = getColorDescription(card.colorIdentity);
-    const colorDescriptive = getColorFullDescription(card.colorIdentity);
-    const colorClass = getColorCategory(card.colorIdentity);    
-    const someColor = getSomeColor(card.colorIdentity);
+      const name = traceryEscape(card.name);
+      const set = traceryEscape(card.set);
+      const rarity = traceryEscape(card.rarity);      
+      const type = randomElement(card.types);
+      const subtype = randomElement(card.subtypes);
+      const fullType = card.types.join(' ');
+      const fullSubtype = card.subtypes && card.types.join(' ');    
+      const someTypeOrSubtype = getSomeCardTypeOrSubtype(card.types, card.subtypes);
+      const imgUrl = traceryEscape(card.imageUrl);
+      const color = getColorDescription(card.colorIdentity);
+      const colorDescriptive = getColorFullDescription(card.colorIdentity);
+      const colorClass = getColorCategory(card.colorIdentity);    
+      const someColor = getSomeColor(card.colorIdentity);
 
-    return ''.concat(
-      `[_cardName1:${name}]`,
-      `[_cardSet1:${set}]`,
-      `[_cardRarity1:${rarity}]`,      
-      `[_cardType1:${type}]`,     
-      `[_cardSubtype1:${subtype}]`,
-      `[_cardFullType1:${fullType}]`,
-      `[_cardFullSubtype1:${fullSubtype}]`,
-      `[_cardSomeTypeOrSubtype1:${someTypeOrSubtype}]`,                              
-      `[_cardImgUrl1:${imgUrl}]`,
-      `[_cardColor1:${color}]`,
-      `[_cardDescriptive1:${colorDescriptive}]`,
-      `[_cardColorClass1:${colorClass}]`,
-      `[_cardSomeColor1:${someColor}]`      
-    );
+      finalResult = finalResult.concat(
+        `[_cardName${i}:${name}]`,
+        `[_cardSet${i}:${set}]`,
+        `[_cardRarity${i}:${rarity}]`,      
+        `[_cardType${i}:${type}]`,     
+        `[_cardSubtype${i}:${subtype}]`,
+        `[_cardFullType${i}:${fullType}]`,
+        `[_cardFullSubtype${i}:${fullSubtype}]`,
+        `[_cardSomeTypeOrSubtype${i}:${someTypeOrSubtype}]`,                              
+        `[_cardImgUrl${i}:${imgUrl}]`,
+        `[_cardColor${i}:${color}]`,
+        `[_cardDescriptive${i}:${colorDescriptive}]`,
+        `[_cardColorClass${i}:${colorClass}]`,
+        `[_cardSomeColor${i}:${someColor}]`      
+      );
+    }
+
+    logger.log('Constructed cardsearch result: ' + finalResult);
+    return finalResult;
   } catch (e) {
     logger.warn(e);
     return 'SEARCH_FAILED_NO_RESULT';
   }
+ 
 }
 
 module.exports = {
   cardSearchBySet,
   cardSearchByText,
-  cardSearchByType,
-  randomStaticCard  
+  cardSearchByType
 };
