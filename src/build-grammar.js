bdd2bd10e1291e4289e7e6b415d163f62221ab8a
Mark Diehr <mdiehr@gmail.com>
2017-08-30 09:54:23 -0400

    Added subdirectory support, reorganized grammar files

--- src/build-grammar.js
+++ src/build-grammar.js
@@ -2,6 +2,7 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 const yaml = require('js-yaml');
 const deepMerge = require(`./lib/util/deep-merge.js`);
 
@@ -9,18 +10,18 @@ const deepMerge = require(`./lib/util/deep-merge.js`);
 // This is synchronous
 function buildGrammar(directory) {
 
-  const files = fs.readdirSync(directory);
-  console.info(`Loading ${files.length} files:\n${files.join(', ')}`);
+  const files = walk(directory);
+  console.info(`Loading ${files.length} grammar files...`);
 
   const flatType = s => s.endsWith(`.txt`);
-  const yamlType = s => s.endsWith(`.json`) || s.endsWith(`.yaml`);
+  const yamlType = s => s.endsWith(`.json`) || s.endsWith(`.yaml`) || s.endsWith(`.yml`);
 
   let destObj = {};
 
   files
     .filter(flatType)
     .map(fileName => {
-      const fileData = fs.readFileSync(`${directory}/${fileName}`);
+      const fileData = fs.readFileSync(`${fileName}`);
       const dataArray = fileData.toString()
         .split(/\r\n/)
         .filter(s => s !== ``);
@@ -33,11 +34,25 @@ function buildGrammar(directory) {
 
   files
     .filter(yamlType)
-    .map(fileName => fs.readFileSync(`${directory}/${fileName}`))
+    .map(fileName => fs.readFileSync(`${fileName}`))
     .map(fileData => yaml.safeLoad(fileData))
     .forEach(doc => destObj = deepMerge(destObj, doc));
 
   return destObj;
 }
+
+const walk = function(dir) {
+  let results = [];
+  const list = fs.readdirSync(dir);
+  list.forEach(function(file) {
+    file = path.join(dir, file);
+    const stat = fs.statSync(file);
+    if (stat && stat.isDirectory())
+      results = results.concat(walk(file));
+    else
+      results.push(file)
+  });
+  return results
+};
 
 module.exports = buildGrammar;
