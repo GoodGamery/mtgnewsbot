3486e695ddff58bc6a4863a5789353d7aff97d28
Mark Diehr <mdiehr@gmail.com>
2017-09-16 14:55:52 -0400

    Fix #171 - Add mastodon integration
    Supply config values for mastodon and --toot

---
+++ src/lib/api/mastodon-client.js
@@ -0,0 +1,25 @@
+// Mastodon is like twitter, but you toot instead of tweet, and the character limit is 500 instead of 140.
+const Masto = require('mastodon');
+const fs = require('fs');
+
+class MastodonClient {
+  constructor(config) {
+    this.mastodon = new Masto({
+      access_token: config.MASTODON_API_TOKEN,
+      timeout_ms: 15*1000,
+      api_url: config.MASTODON_API_URL
+    });
+  }
+
+  async toot(text, mediaPath) {
+    if (mediaPath) {
+      let resp = await this.mastodon.post('media', { file: fs.createReadStream(mediaPath) });
+      let mediaId = resp.data.id;
+      return this.mastodon.post('statuses', { status: `${text}`, media_ids: [mediaId] });
+    } else {
+      return this.mastodon.post('statuses', { status: `${text}` });
+    }
+  }
+}
+
+module.exports = MastodonClient;
