231ced0d207e58aa9ccab402685834d0062251aa
Justin Woolley <justinwoolley@gmail.com>
2017-10-05 02:25:47 -0700

    reply-with-card mtgo-style headlines

--- src/lib/plugins/tracery/modifiers/content-modifiers.js
+++ src/lib/plugins/tracery/modifiers/content-modifiers.js
@@ -7,12 +7,26 @@ function dayOfWeek() {
 function dayOfWeekOccasion() {
   const descriptors = {
     'monday'    : ['Magic', 'Modern', 'Midrange', 'Monocolored', 'Momir', 'Meloku', 'Minotaur', 'Madness', 'Morph', 'Magmaw'],
-    'tuesday'   : ['Tribal', 'Two-Headed', 'Tamiyo', 'Teferi', 'Tutor', 'Tuktuk', 'Tap-Out', 'Trample', 'Takklemaggot', 'True-Name', '0-2'],
+    'tuesday'   : ['Tribal', 'Two-Headed', 'Tamiyo', 'Teferi', 'Tezzeret', 'Tutor', 'Tuktuk', 'Tap-Out', 'Trample', 'Takklemaggot', 'True-Name', '0-2'],
     'wednesday' : ['Weird', 'Whiskey', 'Wizard', 'Welkin', 'Werebear', 'Westvale', 'Wither', 'Withengar', 'Wrexial'],
-    'thursday'  : ['Throwback', 'Thassa', 'Thalia', 'Tezzeret', 'Thopter', 'Thrull', 'Thragtusk', 'Thoughtseize'],
+    'thursday'  : ['Throwback', 'Thassa', 'Thalia', 'Thopter', 'Thrull', 'Thragtusk', 'Thoughtseize'],
     'friday'    : ['Flashback', 'Forgettable', 'Feldon', 'Faerie', 'Frog Tongue', 'Fatty', 'Fat Pants', 'French Vanilla','Fat Pack'],
     'saturday'  : ['Standard', 'Singleton', 'Squirrel', 'Sea Kings\'', 'Satyr', 'Saprazzan', 'Standstill', 'Stasis', 'Storm Scale', 'Shuffler'],
     'sunday'    : ['Stompy', 'Suntail', 'Sun\'s Zenith', 'Sundering', 'Sulfuric', 'Sultai', 'Summoners', 'Spindown', 'Subreddit', 'Slow Play']
+  };
+  const todayDescriptors = descriptors[dayOfWeek().toLowerCase()];
+  return todayDescriptors[Math.floor(Math.random() * todayDescriptors.length)];
+}
+
+function dayOfWeekMotivation() {
+  const descriptors = {
+    'monday'    : ['Motivation', 'Mottos', 'Misconceptions', 'Madness', 'Malarkey', 'Mumbo Jumbo', 'Messages'],
+    'tuesday'   : ['Teachings', 'Talk', 'Tripe', 'Trifles', 'Testimony'],
+    'wednesday' : ['Wisdom', 'Wit', 'Wizardry', 'Wordplay', 'Words'],
+    'thursday'  : ['Thoughts', 'Thinkers'],
+    'friday'    : ['Flavor', 'Fun', 'Faux Pas', 'Foolishness'],
+    'saturday'  : ['Sarcasm', 'Strategy', 'Silliness', 'Sincerity'],
+    'sunday'    : ['Sayings', 'Sophistry', 'Stupidity', 'Stories']
   };
   const todayDescriptors = descriptors[dayOfWeek().toLowerCase()];
   return todayDescriptors[Math.floor(Math.random() * todayDescriptors.length)];
