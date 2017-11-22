2fd4bbd658c38e27ce841aacb4b7934d95aa9d5a
Justin Woolley <justinwoolley@gmail.com>
2017-11-21 19:40:00 -0800

    optional headline parameter for tweet.sh

--- tweet.sh
+++ tweet.sh
@@ -12,11 +12,22 @@ node -v
 # install fonts
 cp ./src/data/fonts/*.ttf /usr/local/share/fonts
 
-# install dependencies
+install dependencies
 npm install --production
 
 # tweet
-node --harmony mtgnews --tweet --toot --discord
+
+if
+  [ -z "$1" ]
+then 
+  # tweet random headline
+  node --harmony mtgnews --tweet --toot --discord
+else
+  # tweet specified headline
+  node --harmony mtgnews --tweet --toot --discord -o "#$1#"
+fi
+
+npm install --production
 
 #silent
 set +x
