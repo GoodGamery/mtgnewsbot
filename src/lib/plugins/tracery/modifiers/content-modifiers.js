2fd22d2ab74fecf017375487f68f91713a4834b4
Justin Woolley <justinwoolley@gmail.com>
2017-12-10 12:03:40 -0800

    randomMonthDay and randomUpcomingDate content modifiers

--- src/lib/plugins/tracery/modifiers/content-modifiers.js
+++ src/lib/plugins/tracery/modifiers/content-modifiers.js
@@ -1,5 +1,25 @@
 'use strict';
 
+// UTILITY FUNCTIONS
+
+function getOrdinalNum(n) {
+  return n + (n > 0 ? ['th', 'st', 'nd', 'rd'][(n > 3 && n < 21) || n % 10 > 3 ? 0 : n % 10] : '');
+}
+
+// returns a random integer between minOrMaxArg and maxArg, or between 1 and minOrMaxArg if maxArg is not defined
+function randomInt(minOrMaxArg, maxArg) {
+  const min = (maxArg !== undefined) ? minOrMaxArg : 1;
+  const max = (maxArg !== undefined) ? maxArg : minOrMaxArg;
+  return Math.floor(Math.random() * (max - min  + 1)) + min;
+}
+
+// returns a string representation of a date in "Month Day" format with ordinal day, e.g. November 18th
+function getMonthDayOrdinal(date) {
+  return date.toLocaleString('en-us', {  month: 'long' }) + ' ' + getOrdinalNum(date.toLocaleString('en-us', {  day: 'numeric' }))
+}
+
+// MODIFIERS
+
 function currentYear() {
   return '' + new Date().getFullYear();
 }
@@ -20,6 +40,20 @@ function dayOfWeekOccasion() {
   };
   const todayDescriptors = descriptors[dayOfWeek().toLowerCase()];
   return '' + todayDescriptors[Math.floor(Math.random() * todayDescriptors.length)];
+}
+
+
+// returns a random month / day other than today's date
+function randomMonthDay() {
+  const date = new Date();
+  date.setDate(date.getDate() + randomInt(1, 364)); // choose a day 7-60 days in future
+  return getMonthDayOrdinal(date);
+}
+
+function randomUpcomingMonthDay() {
+  const date = new Date();
+  date.setDate(date.getDate() + randomInt(7, 60)); // choose a day 7-60 days in future
+  return getMonthDayOrdinal(date);
 }
 
 function dayOfWeekMotivation() {
