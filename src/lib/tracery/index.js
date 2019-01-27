5ced8b338cb163b916cad6cb01d8d8d41b7e4785
jwoolley <19597672+jwoolley@users.noreply.github.com>
2019-01-26 16:30:59 -0800

    new sets and keywords; randomFutureDate; small fixes (#252)

--- src/lib/tracery/index.js
+++ src/lib/tracery/index.js
@@ -801,11 +801,8 @@ var tracery = function() {
         },
 
         firstS : function(s) {
-            console.log(s);
             var s2 = s.split(" ");
-
             var finished = baseEngModifiers.s(s2[0]) + " " + s2.slice(1).join(" ");
-            console.log(finished);
             return finished;
         },
 
